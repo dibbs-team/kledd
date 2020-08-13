387e8d6ce940d7789644fb4e149cb1e15fcb5986
Kal-Elx <kallej1johansson@gmail.com>
2020-08-13 15:08:52 +0200

    setup: internationalization

---
+++ lib/lang/my_localizations.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+import 'l10n/messages_all.dart';
+
+class MyLocalizations {
+  /// Initialize localization systems and messages
+  static Future<MyLocalizations> load(Locale locale) async {
+    // If we're given "en_US", we'll use it as-is. If we're
+    // given "en", we extract it and use it.
+    final String localeName =
+        locale.countryCode == null || locale.countryCode.isEmpty
+            ? locale.languageCode
+            : locale.toString();
+    // We make sure the locale name is in the right format e.g.
+    // converting "en-US" to "en_US".
+    final String canonicalLocaleName = Intl.canonicalizedLocale(localeName);
+    // Load localized messages for the current locale.
+    await initializeMessages(canonicalLocaleName);
+    // Force the locale in Intl.
+    Intl.defaultLocale = canonicalLocaleName;
+    return MyLocalizations();
+  }
+
+  /// Retrieve localization resources for the widget tree
+  /// corresponding to the given `context`
+  static MyLocalizations of(BuildContext context) =>
+      Localizations.of<MyLocalizations>(context, MyLocalizations);
+
+  // ----------------------
+  // Declare messages here.
+  // ----------------------
+
+  String get title => Intl.message(
+        'Kledd',
+        name: 'title',
+        desc: 'App title',
+      );
+
+  String signInButtonText(String service) => Intl.message(
+        'Sign in with $service',
+        name: 'signInButtonText',
+        args: [service],
+        desc: 'Text on sign in button',
+        examples: const {'service': 'Google'},
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'screens/auth_screen.dart';
 import 'screens/splash_screen.dart';
 import 'screens/tab_screen.dart';
+import 'lang/my_localizations.dart';
+import 'lang/my_localizations_delegate.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +16,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kledd',
+      onGenerateTitle: (context) => MyLocalizations.of(context).title,
+      localizationsDelegates: [
+        const MyLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en'), // English (default)
+        const Locale('sv'), // Swedish
+      ],
       theme: ThemeData(
         primarySwatch: Colors.cyan,
         visualDensity: VisualDensity.adaptivePlatformDensity,
