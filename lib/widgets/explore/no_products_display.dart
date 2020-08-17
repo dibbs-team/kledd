aec016bb00ad7531b7256f81cdc2a40418fbc27f
Kal-Elx <kallej1johansson@gmail.com>
2020-08-17 18:42:02 +0200

    feat: pagination in products feed

---
+++ lib/widgets/explore/no_products_display.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NoProductsDisplay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Implement.
+    return Center(
+      child: Text("No products in store"),
+    );
+  }
+}
