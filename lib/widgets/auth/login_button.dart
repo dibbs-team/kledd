ab5b26fd11da8e0797c758d1b7a93647b0312d4e
Johan Ehinger <joheh148@stundent.liu.se>
2020-09-10 18:39:59 +0200

    design: auth_screen redesign
    New design for auth_screen, implemented new library and diffrent
    login buttons for diffrent services.

--- lib/widgets/auth/login_button.dart
+++ lib/widgets/auth/login_button.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:kledd/models/authenticationService.dart';
 
 import '../../lang/my_localizations.dart';
 
 class LoginButton extends StatelessWidget {
-  final String service;
   final Function onLoginUser;
+  final AuthenticationService authenticationService;
 
   LoginButton({
-    this.service,
-    this.onLoginUser,
+    @required this.onLoginUser,
+    @required this.authenticationService,
   });
 
   @override
   Widget build(BuildContext context) {
     final l10n = MyLocalizations.of(context);
 
-    return RaisedButton(
-      onPressed: onLoginUser,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Text(
-        l10n.signInButtonText(service),
-      ),
+    Widget buildLoginBotton() {
+      switch (authenticationService) {
+        case AuthenticationService.GOOGLE:
+          return GoogleSignInButton(
+            onPressed: onLoginUser,
+            text: l10n.signInButtonText('Google'),
+            centered: true,
+          );
+        case AuthenticationService.FACEBOOK:
+          return FacebookSignInButton(
+            onPressed: onLoginUser,
+            text: l10n.signInButtonText('Facebook'),
+            centered: true,
+          );
+        default:
+          return Container();
+      }
+    }
+
+    return Container(
+      width: 300,
+      padding: EdgeInsets.all(5),
+      child: buildLoginBotton(),
     );
   }
 }
