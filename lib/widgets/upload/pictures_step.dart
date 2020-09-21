96b7cdd45d3814da96aebb23a16d281fd4153130
Kalle Johansson <kallej1johansson@gmail.com>
2020-09-21 19:58:10 +0200

    feat: upload photo

--- lib/widgets/upload/pictures_step.dart
+++ lib/widgets/upload/pictures_step.dart
@@ -1,21 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'upload_step.dart';
 import 'upload_title.dart';
+import 'picture_selector.dart';
 import '../../lang/my_localizations.dart';
 
+// ignore: must_be_immutable
 class PicturesStep extends UploadStep {
+  final GlobalKey<PictureSelectorState> _key = GlobalKey();
   final int step;
+  //* Image is mutable because we want to use it to save the image from the
+  //* state of PictureSelector that we can later fetch from the parent.
+  File image;
 
   PicturesStep(this.step) : super(step);
 
-  String get imageUrl =>
-      "https://www.lulus.com/images/product/xlarge/3301380_667902.jpg";
-
   @override
   bool validate() {
-    // TODO: implement validate
-    return true;
+    // TODO: Communicate to user if validation fails.
+
+    // Save image from pictureSelector.
+    image = _key.currentState.image;
+
+    return image != null;
   }
 
   @override
@@ -24,6 +33,9 @@ class PicturesStep extends UploadStep {
     return Column(
       children: [
         UploadTitle(l10n.picturesStepTitle(step)),
+        PictureSelector(
+          key: _key,
+        ),
       ],
     );
   }
